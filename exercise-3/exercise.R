a26f21af33b4942ecab4df69f089472b8ac3fec0
BrydenR <bryden.m.robertson@gmail.com>
2016-10-12 19:03:56 -0700

    Added code for all

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,31 +1,31 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- paste("Employee ", 1:100)
 
 # Create a vector of 2014 salaries using the runif function
-
+sal2014 = runif(100, min=1000, max=10000)
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+sal2015 = runif(100, min=-1000, max=10000)
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries = data.frame('sal.2014'=sal2014, 'sal.2015'=sal2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries.raises <- salaries$sal.2014 - salaries$sal.2015
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries.raises <- salaries$sal.2015 > salaries$sal.2014
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+employee57 = salaries[57,'sal.2015']
 
 # How many employees got a raise?
-
+raises = nrow(salaries[salaries$raises == TRUE,])
 
 # What was the value of the highest raise?
-
+maximum = max(salaries.raises)
 
 # What was the name of the employee who recieved the highest raise?
 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,20 +1,20 @@
 # Exercise 5: Gates Foundation Educational Grants
 
 # Read data into a variable called `grants` using the `read.csv` function
-
+grants = read.csv('./info201/m8-dataframes/exercise-5/data/gates_money.csv')
 
 # Use the View function to look at your data
-
+#View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending = grants[,'total_amount']
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+spending.is.vector <- is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
 # Unfortunately, it will not be a vector by default, so you must transform it using the as.vector function
-
+org <- as.vector(grants['organization'])
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
